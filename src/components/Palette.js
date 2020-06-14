29ce730de2af9bb091a48067ac93138599b91d36
Florencia Gomez <florxgomez@gmail.com>
2020-06-14 01:43:04 -0300

    first commit

---
+++ src/components/Palette.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Palette = () => {
+  return (
+    <div className="palette">
+      {/* navbar */}
+      <div className="palette-colors">{/* color boxes */}</div>
+      {/* footer */}
+    </div>
+  );
+};
+export default Palette;
